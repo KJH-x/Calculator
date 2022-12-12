4291fce328f3e41df2d02b6bcecdeaf05ee124e5
KJH-x <kjh13710362237@outlook.com>
2022-12-12 22:11:03 +0800

    +comments, +structures

--- 累加器.py
+++ 累加器.py
@@ -8,9 +8,12 @@ Author :KJH-x
 
 from traceback import print_exc
 import math
+import sys
 
 
 def output(result: ...) -> None:
+    # 此函数功能目前不佳，在好的替代方案发现之前
+    # 新增的输出内容，直接用print
     if type(result) == "<class 'str'>":
         print(f"==> {result}")
     else:
@@ -53,6 +56,8 @@ def flash(command='part') -> None:
 def convert(expression: str) -> list:
     cache = []
     left = right = 0
+    # 括号优先级计算，左括号加一级，右括号减一级
+    # 输入的括号可不在形式上成对，只要左右个数一致即可
     bracketlevel = 0
     # 输入为空，直接下一轮
     if expression == "":
@@ -63,18 +68,19 @@ def convert(expression: str) -> list:
         print(memory)
     # 输入分析：
     while left < len(expression):
-        append = ""
-        print(f"==interval left={left} right={right}")
+        function = ""
+        # print(f"==interval left={left} right={right}")
         if expression[left].isdigit():
-            # 触发过程量记录
+            # 过程量记录
+            # 调用isfloat函数，把小数吃掉
             for right in range(left+1, len(expression)+1):
 
                 if isfloat(expression[left:right]):
-                    append = float(expression[left:right])
+                    function = float(expression[left:right])
                 else:
                     right -= 1
                     break
-            cache.append(append)
+            cache.append(function)
             last_type = "number"
             left = right
             continue
@@ -82,6 +88,7 @@ def convert(expression: str) -> list:
         elif expression[left] in rightclass:
             if expression[left] == '!':
                 # 阶乘立刻计算
+                # 后续可能需要改进，这将导致能约分的出现故障
                 cache.append(math.factorial(cache.pop()))
             last_type = "number"
             left += 1
@@ -97,35 +104,48 @@ def convert(expression: str) -> list:
 
         elif expression[left].isalpha():
             # 记录函数
+            # 调用isfunction来把一串英文和后随括号给吃掉
             for right in range(left+1, len(expression)+1):
                 if isfunction(expression[left:right]):
-                    append = [expression[left:right], bracketlevel]
+                    function = [expression[left:right], bracketlevel]
                 else:
                     right -= 1
                     break
-            cache.append(append)
+            if function[0][-1] not in Lbracket:
+                print(f"[Warn] {function} is not a function, skipped")
+
+            # 记录的时候记录函数名称
+            cache.append(function)
             last_type = "function"
             left = right
             bracketlevel += 1
             continue
 
         elif expression[left] in Lbracket:
+            # 遇到左侧等号，处理后加一级
             cache.append([str(expression)[left], bracketlevel])
             left += 1
             bracketlevel += 1
             continue
 
         elif expression[left] in Rbracket:
+            # 遇到右侧等号，处理前减一级
             bracketlevel -= 1
             cache.append([str(expression)[left], bracketlevel])
             left += 1
             continue
 
         elif expression[left] in [",", "，"]:
+            # 分隔符在结构中已经被不同位置的数字等效，故跳过
             left += 1
             continue
+
+        else:
+            print(f"[Warn] IN {left+1}, cannot resolve, skipped.")
+            left += 1
+
     if bracketlevel != 0:
-        print("bracket not close!")
+        print("\n\t[ERROR] Bracket not closed!\n")
     return cache
 
 
@@ -139,10 +159,28 @@ def solve(cache: list) -> float:
     return 0
 
 
+def a_mode(expression: str) -> ...:
+    l = sys._getframe().f_code.co_name
+    print(l)
+    input(l+">")
+    return
+
+
+def special_mode(mode: str, expression: str) -> ...:
+    # 此处用来放置需要特殊使用的模式
+    # 比如无限求和的近似、矩阵高级运算等等
+    if mode == "a_mode":
+        print("\n\ta_mode activated:")
+        a_mode(expression)
+        print("\n\ta_mode exiting...:")
+    return
+
+
 midclass = ['+', '-', '*', '/', '%', '^', 'A', 'P', 'C',]
 rightclass = ['!']
-Lbracket = ['(', '[', '{', '（', '【']
-Rbracket = [')', ']', '}', '）',  '】']
+# changelog:[]will no longer be used as general brackets
+Lbracket = ['(', '{', '（', '【']
+Rbracket = [')', '}', '）',  '】']
 
 level = [
     ["nothing"],
@@ -152,9 +190,13 @@ level = [
     ["A", "P", "C"],
     ['(', ')', '[', ']', '{', '}', '（', '）', '【', '】']
 ]
+
+
 Ans = 0
 last = 0
 history = []
+
+
 if __name__ == '__main__':
 
     memory = []
@@ -163,14 +205,30 @@ if __name__ == '__main__':
     while True:
         try:
             # 输入，去除空格和前后空白符
-            expression = input().strip().replace(" ", "")
-            cache = convert(expression)
-            print("==>cache:")
-            output(cache)
-            if expression.count("=") == 0:
-                calculate(cache)
+            expression = input("Basic> ").strip().replace(" ", "")
+            # Assume that the mode command should act like:
+            # MODE:<><mode_name><space|tab><input|none>
+            if expression[0:5].lower() == "mode:":
+                i = 0
+                for i in range(5, len(expression)):
+                    if expression[i] in [' ', '\t']:
+                        i -= 1
+                        break
+                mode = expression[5:i+1]
+                if i+1 == len(expression):
+                    special_mode(mode, "")
+                else:
+                    special_mode(mode, expression[i:].lower())
             else:
-                solve(cache)
+                # 输入主程序，此步获得输入的完整结构信息
+                cache = convert(expression)
+
+                print("==>cache:")
+                output(cache)
+                if expression.count("=") == 0:
+                    calculate(cache)
+                else:
+                    solve(cache)
 
         except EOFError:
             Ans = history.pop()
